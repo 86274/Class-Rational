db0f3c25267dae450fe8d271fac6d262446bc75c
86274 <pochta.mame10@gmail.com>
2021-12-15 19:50:40 +0300

    Create ClassRational.cpp

---
+++ ClassRational.cpp
@@ -0,0 +1,272 @@
+#include <iostream>
+#include <exception>
+#include <numeric>
+
+using namespace std;
+
+class Rational
+{
+public:
+	Rational();
+	Rational(int numerator, int denominator);
+
+	int Numerator() const;
+	int Denominator() const;
+
+	Rational operator + (const Rational& y);
+	Rational sumRational(Rational x, Rational y);
+	Rational operator - (const Rational& y);
+	Rational diffRational(Rational x, Rational y);
+	Rational operator * (const Rational& y);
+	Rational Multiplication(Rational x, Rational y);
+	Rational operator / (const Rational& y);
+	Rational divisionRational(Rational x, Rational y);
+
+	friend bool operator > (const Rational& x, const Rational& y);
+	friend bool operator < (const Rational& x, const Rational& y);
+	friend bool operator== (const Rational& x, const Rational& y);
+	friend std::istream& operator >> (std::istream& is, Rational& x);
+	friend std::ostream& operator << (std::ostream& os, Rational x);
+
+private:
+	int p;
+	int q;
+
+	//Проверка положительное число или нет
+	//Если дробь p / q отрицательная, то объект Rational(p, q) должен иметь отрицательный числитель и положительный знаменатель.
+	void checkZnak();
+	//Сокращение дроби 
+	void reduction();
+	//Приведение к общему знаменателю
+	void equalDenominator(Rational& x, Rational& y);
+
+};
+
+
+Rational::Rational()
+{
+	p = 0;
+	q = 1;
+}
+
+Rational::Rational(int numerator, int denominator)
+{
+	if (denominator == 0)
+	{
+		throw invalid_argument("invalid argument");
+	}
+	p = numerator;
+	q = denominator;
+	checkZnak();
+	reduction();
+}
+
+int Rational::Numerator() const
+{
+	return p;
+}
+
+int Rational::Denominator() const
+{
+	return q;
+}
+
+Rational Rational::operator+(const Rational& y)
+{
+	return sumRational(*this, y);
+}
+
+Rational Rational::sumRational(Rational x, Rational y)
+{
+	Rational temp;
+	equalDenominator(x, y);
+	temp.p = x.p + y.p;
+	temp.q = x.q;
+	temp.reduction();
+	return temp;
+}
+
+Rational Rational::operator-(const Rational& y)
+{
+	return diffRational(*this, y);
+}
+
+Rational Rational::diffRational(Rational x, Rational y)
+{
+	Rational temp;
+	equalDenominator(x, y);
+	temp.p = x.p - y.p;
+	temp.q = x.q;
+	temp.checkZnak();
+	temp.reduction();
+	return temp;
+}
+
+bool operator == (const Rational& x, const Rational& y)
+{
+	return x.p == y.p && x.q == y.q;
+}
+
+Rational operator*(const Rational& x, const Rational& y)
+{
+	return x * y;
+}
+
+Rational Rational::operator*(const Rational& y)
+{
+	return Multiplication(*this, y);
+}
+
+Rational Rational::Multiplication(Rational x, Rational y)
+{
+	Rational temp;
+	temp.p = x.p * y.p;
+	temp.q = x.q * y.q;
+	temp.checkZnak();
+	temp.reduction();
+	return temp;
+}
+
+Rational operator/(const Rational& x, const Rational& y)
+{
+	return x / y;
+}
+
+Rational Rational::operator/(const Rational& y)
+{
+	if (y.Numerator() == 0)
+	{
+		throw domain_error("");
+	}
+	return divisionRational(*this, y);
+}
+
+Rational Rational::divisionRational(Rational x, Rational y)
+{
+	Rational temp;
+	temp.p = x.p * y.q;
+	temp.q = x.q * y.p;
+	temp.checkZnak();
+	temp.reduction();
+	return temp;
+	return Rational();
+}
+
+bool operator < (const Rational& x, const Rational& y)
+{
+	if (x.p * y.q < y.p * x.q)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool operator > (const Rational& x, const Rational& y)
+{
+	if (x.p * y.q < y.p * x.q)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void Rational::checkZnak()
+{
+	if (p < 0 && q < 0)
+	{
+		p = abs(p);
+		q = abs(q);
+	}
+	else if (q < 0)
+	{
+		p = -p;
+		q = abs(q);
+	}
+}
+
+void Rational::reduction()
+{
+	int divider = std::gcd(p, q);
+	if (divider != 1)
+	{
+		p = p / divider;
+		q = q / divider;
+	}
+}
+
+void Rational::equalDenominator(Rational& x, Rational& y)
+{
+	x.p *= y.q;
+	y.p *= x.q;
+	x.q = y.q = x.q * y.q;
+}
+
+std::istream& operator >> (std::istream& is, Rational& x)
+{
+	int buf = 0, buf2 = 0;
+	if (is >> buf)
+	{
+		char c;
+		is >> c;
+		if (c == '/')
+		{
+			if (is >> buf2) {
+				x.p = buf;
+				x.q = buf2;
+				x.checkZnak();
+				x.reduction();
+				return is;
+			}
+			else
+			{
+				return is;
+			}
+
+		}
+		else
+		{
+			return is;
+		}
+	}
+	else
+	{
+		return is;
+	}
+
+
+}
+
+std::ostream& operator << (std::ostream& os, Rational x)
+{
+	os << x.p << "/" << x.q;
+	return os;
+}
+
+
+int main() {
+
+	try {
+		Rational r(1, 0);
+		cout << "Doesn't throw in case of zero denominator" << endl;
+		return 1;
+	}
+	catch (invalid_argument& ex) {
+	}
+
+	try {
+		auto x = Rational(1, 2) / Rational(0, 1);
+		cout << "Doesn't throw in case of division by zero" << endl;
+		return 2;
+	}
+	catch (domain_error&) {
+	}
+
+	cout << "OK" << endl;
+	return 0;
+}
